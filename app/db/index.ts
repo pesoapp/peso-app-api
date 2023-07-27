45631e23062aa6a16225e6956582103dae1407c3
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-07-27 10:49:37 +0800

    Add prisma in db

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,19 +1,4 @@
-import { Sequelize, DataTypes } from "sequelize";
-import ENV from "../env";
-const sequelize = new Sequelize(ENV.DB_NAME, ENV.DB_USER, ENV.DB_PASSWORD, {
-  host: "localhost",
-  dialect: "mysql",
-});
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-async function connectDB() {
-  try {
-    await sequelize.authenticate();
-    console.log(
-      `✅ Connection to ${ENV.DATABASE_URL} has been established successfully.`
-    );
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-}
-
-export { connectDB, sequelize, Sequelize, DataTypes };
+export { prisma };
