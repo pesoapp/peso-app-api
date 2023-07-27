e02e739e4865835aac618adb8a03b2768c7fea55
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-07-27 10:50:58 +0800

    Update query in auction service

--- app/routes/v1/auction/service.ts
+++ app/routes/v1/auction/service.ts
@@ -1,13 +1,11 @@
-import { sequelize } from "../../../db";
+import { prisma } from "../../../db";
 
 const getAll = async (_query: any) => {
-  let query =
-    "SELECT a.*, oc.firstname, oc.lastname, c.condition, FORMAT(a.price,2) AS priceFormat, LOWER(REPLACE(REPLACE(a.name,' ','-'),'/','-')) AS href, oc.picture AS customer_picture, DATE_FORMAT(a.due_date,'%M %d, %Y') AS due FROM `auction` a LEFT JOIN oc_customer oc ON a.customer_id=oc.customer_id LEFT JOIN `condition` c ON a.condition_id=c.id WHERE deleted_at IS NULL ";
-  query += " ORDER BY RAND()";
-  query += ` LIMIT ${_query.page - 1}, ${_query.limit}`;
-  const [results, metadata] = await sequelize.query(query);
-
-  return results;
+  return await prisma.auction.findMany({
+    include: {
+      condition: true,
+    },
+  });
 };
 
 const add = async (_body: any, session: any) => {};
