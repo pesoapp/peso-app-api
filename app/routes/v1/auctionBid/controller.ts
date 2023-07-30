847c37236917ead1f55849cbc2a237c3d6b7a8ce
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-07-30 22:21:28 +0800

    Add getByAuction in auctionBid route

--- app/routes/v1/auctionBid/controller.ts
+++ app/routes/v1/auctionBid/controller.ts
@@ -1,5 +1,6 @@
 import service from "./service";
 import { Request, Response } from "express";
+import ocCustomer from "../ocCustomer/service";
 
 const getAll = async (_req: Request, _res: Response) => {
   const { limit = 5, page = 1 } = _req.query;
@@ -25,6 +26,28 @@ const getById = async (_req: Request, _res: Response) => {
   });
 };
 
+const getByAuction = async (_req: Request, _res: Response) => {
+  const { id = 0 } = _req.params;
+  const data = await service.getByAuction(Number(id));
+
+  const temp =
+    (await ocCustomer.getManyByCustomer(data.map((e: any) => e.customer_id))) ??
+    [];
+
+  data.map((e: any) => {
+    e.customer = temp.find(
+      (customer: any) => (customer.customer_id = e.customer_id)
+    );
+    return e;
+  });
+
+  _res.send({
+    data,
+    status: "success",
+    message: "Get Auction Bid success",
+  });
+};
+
 const add = async (_req: Request, _res: Response) => {
   const data = await service.add(_req.body);
   _res.send({
@@ -38,4 +61,4 @@ const update = async (_req: Request, _res: Response) => {};
 
 const removeOne = async (_req: Request, _res: Response) => {};
 
-export { getAll, getById, add, update, removeOne };
+export { getByAuction, getAll, getById, add, update, removeOne };
