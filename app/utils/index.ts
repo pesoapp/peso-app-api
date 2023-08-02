c3e409a59b2e6e7c89a773b0ae90ffecc25ec0b5
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-08-02 17:34:29 +0800

    Add S3_INSTANCE in utils

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,3 +1,11 @@
+import AWS from "aws-sdk";
+import ENV from "../env";
+
+export const S3_INSTANCE = new AWS.S3({
+  accessKeyId: ENV.AWS_ACCESS_KEY_ID,
+  secretAccessKey: ENV.AWS_SECRET_ACCESS_KEY,
+});
+
 export function generateId(length: number = 5) {
   let result = "";
   const characters =
