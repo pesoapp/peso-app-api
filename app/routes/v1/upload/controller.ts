450d812335df83efc96dad2abcf973341970a4b8
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-08-02 19:20:24 +0800

    Add peso app upload auction item

--- app/routes/v1/upload/controller.ts
+++ app/routes/v1/upload/controller.ts
@@ -2,19 +2,33 @@ import { Request, Response } from "express";
 import { S3_INSTANCE, generateId } from "../../../utils";
 import ENV from "../../../env";
 import fs from "fs";
+import { readFile } from "node:fs/promises";
 
 const addAuction = async (_req: Request, _res: Response) => {
-  const name: string = generateId(20);
-  await S3_INSTANCE.upload({
-    Bucket: ENV.AWS_BUCKET_NAME,
-    Key: `auction/${name}`,
-    Body: _req.file?.path || "",
-  }).promise();
+  // AWS FILE UPLOAD
+  // const name: string = generateId(20);
+  // await S3_INSTANCE.upload({
+  //   Bucket: ENV.AWS_BUCKET_NAME,
+  //   Key: `auction/${name}`,
+  //   Body: _req.file?.path || "",
+  // }).promise();
+
+  const image = new Blob([await readFile(_req.file?.path ?? "")]);
+  let formData = new FormData();
+  formData.set("imageMain", image, _req.file?.path ?? "");
+  const resp = await fetch(
+    `${ENV.PESO_APP_TEMP}auction.php?action=uploadAuctionImage`,
+    {
+      body: formData,
+      method: "post",
+    }
+  );
+  const path = JSON.parse(await resp.text()).path;
 
   await fs.unlink(_req.file?.path || "", () => {});
 
   _res.send({
-    data: [{ path: `auction/${name}` }],
+    data: [{ path }],
     status: "success",
     message: "Upload Auction Image success",
   });
