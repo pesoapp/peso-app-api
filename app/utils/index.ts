f4a1849b060027bda2dc9b0089e892f3af52ce8e
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-08-07 13:45:59 +0800

    Add youtubeParser in utils

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -42,3 +42,10 @@ export function parseLoungePostTitle(title: string = "") {
   title = title.trim();
   return title;
 }
+
+export function youtubeParser(url: string) {
+  var regExp =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  var match = url.match(regExp);
+  return match && match[7].length == 11 ? match[7] : false;
+}
