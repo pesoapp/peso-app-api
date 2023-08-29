dfb014154397c1241a2a1650d3a6ecca74d54cb8
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-08-29 15:28:17 +0800

    Update getManyByCustomer

--- app/routes/v1/ocCart/controller.ts
+++ app/routes/v1/ocCart/controller.ts
@@ -1,9 +1,7 @@
 import service from "./service";
-import ocSeller from "../ocSeller/service";
-import ocProduct from "../ocProduct/service";
-import bgProduct from "../bgProduct/service";
-import { Request, Response } from "express";
 
+import { Request, Response } from "express";
+import { pTypeParser } from "../../../utils";
 const getAll = async (_req: Request, _res: Response) => {
   const { limit = 10, page = 1 } = _req.query;
   const data = await service.getAll({});
@@ -19,50 +17,244 @@ const getAll = async (_req: Request, _res: Response) => {
   });
 };
 
+// TODO: add bg products
+// TODO: product_category_names in reg
+// TODO: brandName in reg
+// TODO: getShippingAmoutByProduct in reg
 const getManyByCustomer = async (_req: Request, _res: Response) => {
   const { id = 0 } = _req.params;
+
+  // 1. getManyByCustomer
   const data = await service.getManyByCustomer(Number(id));
-  const ocSellerTemp = await ocSeller.getManyBySeller(
-    data.map((e: any) => e.seller_id)
-  );
 
-  const ocProductTemp = await ocProduct.getManyById(
-    data.map((e: any) => e.product_id)
-  );
+  // 2. Update ocCart quantity to 1 from getManyByCustomer when quantity is 0
+  await service.clearQuantity(Number(id));
 
-  const bgProductTemp = await bgProduct.getManyByProducts(
-    ocProductTemp.map((e: any) => e.product_id)
-  );
-  console.log(bgProductTemp);
-
-  ocProductTemp.map((e: any) => {
-    e.bg = bgProductTemp.find((bg: any) => bg.product_id == e.product_id) ?? {};
+  // 3. Parse p type
+  const parsedPType = data.map((e: any) => {
+    e.type = e.p_type;
+    e.p_type = pTypeParser(e.p_type);
     return e;
   });
 
-  data.map((e: any) => {
-    e.seller =
-      ocSellerTemp.find((seller: any) => seller.seller_id == e.seller_id) ??
-      null;
+  // 4. Get single products (oc product and oc product descriptionn)
+  const productDetails = await service.getByProduct(
+    parsedPType.map((e: any) => e.product_id),
+    []
+  );
 
-    e.product =
-      ocProductTemp.find(
-        (product: any) => product.product_id == e.product_id
-      ) ?? null;
+  const productDetailsTemp = productDetails.map((product: any) => {
+    const tempParsed = parsedPType.find(
+      (e: any) => e.product_id == product.product_id
+    );
+    if (!tempParsed) return {};
+    return { ...tempParsed, product };
+  });
 
-    if (e.product == null || e.seller == null) {
-      return null;
-    }
+  // 5. Parse old price and price
 
+  // 6. Add product quantity branch from seller_branch_selected_products as productQuantity
+  const productQuantities = await service.getQuantityByProduct(
+    [...new Set(productDetailsTemp.map((e: any) => e.seller_id))],
+    [...new Set(productDetailsTemp.map((e: any) => e.product.product_id))],
+    [...new Set(productDetailsTemp.map((e: any) => e.branch_id))]
+  );
+
+  const productQuantitiesTemp = productDetailsTemp.map((e: any) => {
+    const temp = productQuantities.find((quantity: any) => {
+      return (
+        quantity.seller_id == e.seller_id &&
+        quantity.branch_id == e.branch_id &&
+        quantity.product_id == e.product.product_id
+      );
+    });
+    return {
+      ...e,
+      productQuantity: temp.quantity,
+      quantity: temp.quantity > 0 ? e.quantity : 0,
+    };
+  });
+
+  // 7. Get Branch and Seller names
+  const branchNames = await service.getManySellerBranchById([
+    ...new Set(productQuantitiesTemp.map((e: any) => e.branch_id)),
+  ]);
+
+  const sellerNames = await service.getManySellerNamesById([
+    ...new Set(productQuantitiesTemp.map((e: any) => e.seller_id)),
+  ]);
+
+  const parsedSellerNames = productQuantitiesTemp.map((e: any) => {
+    const tempBranch =
+      branchNames.find((branch: any) => branch.id == e.branch_id) ?? {};
+
+    const tempSeller = sellerNames.find(
+      (seller: any) => seller.seller_id == e.seller_id
+    );
+
+    return {
+      ...e,
+      branchName: tempBranch.b_name ?? tempSeller.shop_name,
+      seller: tempSeller,
+    };
+  });
+
+  // 8. Get Product discounts
+  const discounts = await service.getManyProductDiscountById([
+    ...new Set(parsedSellerNames.map((e: any) => e.product_id)),
+  ]);
+
+  const discountsTemp = parsedSellerNames.map((e: any) => {
+    e.discount = discounts.find(
+      (discount: any) => e.product_id == discount.product_id
+    );
     return e;
   });
 
   _res.send({
-    data,
+    data: discountsTemp,
     status: "success",
     message: "Get Oc Cart success",
   });
+
+  // 5. parse product by p_type
+  // const temp = productDetails.map((e: any) => {
+  //   return e
+  // })
+
+  // const temp: any[] = data.map(async (e: any) => {
+
+  //   const type = pTypeParser(e.p_type);
+  //   const productInfo = (await service.getByProduct(Number(e.product_id), pTypeParser(e.p_type)))[0]
+
+  //   if (productInfo) {
+  //     if (productInfo.type == "reg") {
+  //       const special = false
+  //       let price = e['price'];
+  //       let oldPrice = productInfo['price'];
+
+  //       if (e['price'] != 0 || e['price'] != null) {
+  //         if (e['price'] != productInfo['price']) {
+  //           price = e['price'];
+  //           oldPrice = productInfo['price'];
+  //         } else {
+  //           price = productInfo['price'];
+  //           oldPrice = 0;
+  //         }
+  //       } else {
+  //         price = productInfo['price'];
+  //         oldPrice = 0;
+  //       }
+  //       const productQuantity = (await sellerBranchSelectedProducts.getBranchQuantity(e['seller_id'], productInfo['product_id'], e['branch_id']))[0].quantity ?? 0;
+  //       const quantity = productQuantity > 0 ? e['quantity'] : 0;
+  //       const branchName = (await sellerBranch.getNameById(e['branch_id']))[0] ?? "";
+  //       const seller = (await ocSeller.getById(e['seller_id']))[0] ?? {};
+  //       // 'id' => (int)$cart['cart_id'],
+  //       //   'productId' => $product_info['product_id'],
+  //       //     'branchId' => $cart['branch_id'],
+  //       //       'name' => utf8_encode($product_info['name']),
+  //       //         'quantity' => $quantity,
+  //       //           'productQuantity' => $product_quantity,
+  //       //             'price' => (float) $price,
+  //       //               'oldPrice' => (float) $oldPrice,
+  //       //                 'total' => ($price * $quantity),
+  //       //                   'thumb' => $product_info['thumb'],
+  //       //                     'special' => $special,
+  //       //                       'discount' => $this -> cart_discounts($cart['product_id']),
+  //       //                         'type' => $cart['p_type'],
+  //       //                           'typeCode' => $product_info['type'],
+  //       //                             'warehouse' => [],
+  //       //                               'freebies' => $cart['freebies'],
+  //       //                                 'branchName' => $branchName !== '' ? $branchName : $seller['shopName'],
+  //       //                                   'seller' => $seller,
+  //       //                                     'category' => $product -> product_category_names($product_info['product_id']),
+  //       //                                       'brand' => $product -> brandName($product_info['product_id']),
+  //       //                                         'shippingValue' => $product -> getShippingAmoutByProduct($product_info['product_id'])
+  //       return {
+  //         id: e['cart_id'],
+  //         productId: productInfo['product_id'],
+  //         branchId: productQuantity,
+  //         name: productInfo['name'],
+  //         quantity: e['cart_id'],
+  //         productQuantity: productQuantity,
+  //         price: price,
+  //         oldPrice: oldPrice,
+  //         total: price * quantity,
+  //         thumb: productInfo['thumb'],
+  //         special: special,
+  //         discount: e['cart_id'],
+  //         type: e['cart_id'],
+  //         typeCode: e['cart_id'],
+  //         warehouse: e['cart_id'],
+  //         freebies: e['cart_id'],
+  //         branchName: e['cart_id'],
+  //       }
+  //     }
+  //   }
+
+  //   return {}
+  // })
+
+  // console.log(await Promise.all(temp));
+
+  // const products = await Promise.all(data.map(async (e: any) => {
+  //   return (await service.getByProduct(Number(e.product_id), pTypeParser(e.p_type)))[0]
+  // }))
+
+  // products.map((e: any) => {
+  //   if (e.type == 'reg' ){
+  //     e.special = false;
+  //     data
+  //   }
+  //   return e;
+  // })
+
+  // console.log(products);
 };
+
+// const getManyByCustomer = async (_req: Request, _res: Response) => {
+//   const { id = 0 } = _req.params;
+//   const data = await service.getManyByCustomer(Number(id));
+//   const ocSellerTemp = await ocSeller.getManyBySeller(
+//     data.map((e: any) => e.seller_id)
+//   );
+
+//   const ocProductTemp = await ocProduct.getManyById(
+//     data.map((e: any) => e.product_id)
+//   );
+
+//   const bgProductTemp = await bgProduct.getManyByProducts(
+//     ocProductTemp.map((e: any) => e.product_id)
+//   );
+
+//   ocProductTemp.map((e: any) => {
+//     e.bg = bgProductTemp.find((bg: any) => bg.product_id == e.product_id) ?? {};
+//     return e;
+//   });
+
+//   data.map((e: any) => {
+//     e.seller =
+//       ocSellerTemp.find((seller: any) => seller.seller_id == e.seller_id) ??
+//       null;
+
+//     e.product =
+//       ocProductTemp.find(
+//         (product: any) => product.product_id == e.product_id
+//       ) ?? null;
+
+//     if (e.product == null || e.seller == null) {
+//       return null;
+//     }
+
+//     return e;
+//   });
+
+//   _res.send({
+//     data,
+//     status: "success",
+//     message: "Get Oc Cart success",
+//   });
+// };
 
 const getById = async (_req: Request, _res: Response) => {
   const { id = 0 } = _req.params;
--- app/routes/v1/ocCart/service.ts
+++ app/routes/v1/ocCart/service.ts
@@ -12,6 +12,60 @@ const getManyByCustomer = async (id: number) => {
   });
 };
 
+const clearQuantity = async (customer_id: number) => {
+  return await prisma.$executeRawUnsafe<any[]>(
+    "UPDATE oc_cart SET quantity = 1 WHERE customer_id = " +
+      customer_id +
+      " AND quantity < 0 AND seller_id IS NOT NULL;"
+  );
+};
+
+const getByProduct = async (product_ids: number[], type: any) => {
+  return await prisma.$queryRawUnsafe<any[]>(
+    "SELECT * FROM (SELECT DISTINCT p.product_id, p.price, pd.name AS name, p.image, p.status, 'reg' as `type`, p.model FROM oc_product p  LEFT JOIN oc_product_description pd ON(p.product_id = pd.product_id)  UNION All SELECT bg.product_id, bg.price, bg.product_name as name, bg.img as image, bg.status, 'bg' as `type`, bg.product_id as model FROM bg_product bg ) as pp  WHERE pp.status = 1 AND pp.product_id IN (" +
+      product_ids +
+      ") AND pp.type IN ('bg', 'reg');"
+  );
+};
+
+const getQuantityByProduct = async (
+  product_ids: number[],
+  seller_ids: number[],
+  branch_id: number[]
+) => {
+  return await prisma.$queryRawUnsafe<any[]>(
+    "SELECT * FROM seller_branch_selected_products WHERE product_id IN (" +
+      product_ids +
+      ") OR seller_id IN (" +
+      seller_ids +
+      ") OR branch_id IN (" +
+      branch_id +
+      ");"
+  );
+};
+
+const getManySellerBranchById = async (ids: number[]) => {
+  return await prisma.$queryRawUnsafe<any[]>(
+    "SELECT * FROM seller_branch where id IN (" + ids + ")"
+  );
+};
+
+const getManySellerNamesById = async (ids: number[]) => {
+  return await prisma.$queryRawUnsafe<any[]>(
+    "SELECT s.seller_id, s.shop_name, concat('company/', fp.flagship_logo) as image, fp.theme_color FROM oc_seller s LEFT JOIN flagship_profile fp ON fp.seller_id = s.seller_id WHERE s.seller_id IN (" +
+      ids +
+      ") LIMIT 1"
+  );
+};
+
+const getManyProductDiscountById = async (ids: number[]) => {
+  return await prisma.$queryRawUnsafe<any[]>(
+    "SELECT * FROM oc_product_discount WHERE product_id IN (" +
+      ids +
+      ") AND quantity > 1 AND ((date_start = null OR date_start < convert_tz(utc_timestamp(),'-08:00','+0:00')) AND (date_end = null OR date_end > convert_tz(utc_timestamp(),'-08:00','+0:00'))) ORDER BY quantity ASC, priority ASC, price ASC"
+  );
+};
+
 const getById = async (id: number) => {
   return await prisma.oc_cart.findFirst({});
 };
@@ -22,4 +76,17 @@ const update = async (id: number, _body: any) => {};
 
 const removeOne = async (id: number) => {};
 
-export default { getManyByCustomer, getAll, add, update, removeOne, getById };
+export default {
+  getManyProductDiscountById,
+  getManySellerNamesById,
+  getManySellerBranchById,
+  getQuantityByProduct,
+  getByProduct,
+  clearQuantity,
+  getManyByCustomer,
+  getAll,
+  add,
+  update,
+  removeOne,
+  getById,
+};
