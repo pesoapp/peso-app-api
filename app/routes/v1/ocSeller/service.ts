eb49c011ba1cc8681080087dac7aa0c991fda597
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-08-29 15:27:50 +0800

    Add getById in ocSeller

--- app/routes/v1/ocSeller/service.ts
+++ app/routes/v1/ocSeller/service.ts
@@ -12,12 +12,17 @@ const getManyBySeller = async (ids: number[]) => {
   });
 };
 
-const getById = async (id: number) => {};
 
-const add = async (_body: any) => {};
+const getById = async (seller_id: number) => {
+  return await prisma.$queryRawUnsafe<any[]>(
+    "SELECT s.seller_id, s.shop_name, concat('company/', fp.flagship_logo) as image, fp.theme_color FROM oc_seller s LEFT JOIN flagship_profile fp ON fp.seller_id = s.seller_id WHERE s.seller_id = " + seller_id + " LIMIT 1;"
+  );
+};
 
-const update = async (filter: any, _body: any, session: any) => {};
+const add = async (_body: any) => { };
 
-const removeOne = async (id: number) => {};
+const update = async (filter: any, _body: any, session: any) => { };
+
+const removeOne = async (id: number) => { };
 
 export default { getManyBySeller, getAll, add, update, removeOne, getById };
