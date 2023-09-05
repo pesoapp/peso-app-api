60cf22a7b0f19b2ad7270b44221a2c9102bcb1a3
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-09-05 16:38:04 +0800

    Update error middleware

--- app/middlewares/error.middleware.ts
+++ app/middlewares/error.middleware.ts
@@ -1,18 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 
-const middleware = (_req: Request, _res: Response, next: NextFunction) => {
-  try {
-    next();
-  } catch (_: any) {
-    console.log("test");
-
-    _res.send({
-      data: [],
-      status: "failed",
-      message: _.toString(),
-    });
-    return;
+const middleware = (
+  err: any,
+  _req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  if (_res.headersSent) {
+    return next(err);
   }
+  console.log(1);
+
+  _res.send({
+    data: [],
+    status: "fail",
+    message: "There seems to be a problem with the server",
+  });
 };
 
 export default middleware;
