ed91f76e08a1896e88ea840dc29c931f38a32cdb
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-09-08 21:52:30 +0800

    Add getDRRate

--- app/routes/v1/ocDeliveryCharge/service.ts
+++ app/routes/v1/ocDeliveryCharge/service.ts
@@ -3,6 +3,7 @@ import { prisma } from "../../../db";
 const getAll = async () => {
   return await prisma.oc_delivery_charge.findMany();
 };
+
 const getManyById = async (ids: number[]) => {
   return await prisma.oc_delivery_charge.findMany({
     where: {
@@ -19,10 +20,25 @@ const getById = async (id: number) => {
   });
 };
 
+const getDRRate = async () => {
+  return await prisma.oc_delivery_charge.findMany({
+    where: {
+      id: { in: [1, 2, 3, 5] },
+    },
+  });
+};
 const add = async (_body: any, session: any) => {};
 
 const update = async (filter: any, _body: any, session: any) => {};
 
 const removeOne = async (filter: any, session: any) => {};
 
-export default { getManyById, getById, getAll, add, update, removeOne };
+export default {
+  getDRRate,
+  getManyById,
+  getById,
+  getAll,
+  add,
+  update,
+  removeOne,
+};
