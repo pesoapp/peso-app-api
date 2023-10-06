22df1559225a021b48ca4c771139df12dba77501
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-06 23:49:43 +0800

    Update addressTracker

--- app/routes/v1/addressTracker/controller.ts
+++ app/routes/v1/addressTracker/controller.ts
@@ -3,22 +3,46 @@ import { Request, Response } from "express";
 
 const getAll = async (_req: Request, _res: Response) => {
   const { limit = 10, page = 1, province = "", city = "" } = _req.query;
-  const data = await service.getAll({
-    limit: Number(limit),
-    page: Number(page),
-    province: province.toString(),
-    city: city.toString(),
-  });
-
-  _res.send({
-    data,
-    status: "success",
-    message: "Get Address Tracker success",
+  let response: any = {
+    data: [],
+    status: "fail",
+    message: "Get Address Tracker failed",
     meta: {
       currentPage: Number(page),
       limit: Number(limit),
     },
-  });
+  };
+
+  try {
+    const data = await service.getAll({
+      limit: Number(limit),
+      page: Number(page),
+      province: province.toString(),
+      city: city.toString(),
+    });
+
+    response = {
+      data: data,
+      status: "success",
+      message: "Get Address Tracker success",
+      meta: {
+        currentPage: Number(page),
+        limit: Number(limit),
+      },
+    };
+  } catch (_) {
+    response = {
+      data: [],
+      status: "fail",
+      message: "Get Address Tracker failed",
+      meta: {
+        currentPage: Number(page),
+        limit: Number(limit),
+      },
+    };
+  }
+
+  _res.send(response);
 };
 
 const getById = async (_req: Request, _res: Response) => {
